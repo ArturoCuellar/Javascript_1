6be9be145be72f2a4dca6dd55fd4d37dc72de2af
ArturoCuellar <arturocuellarm3@gmail.com>
2022-01-11 17:07:40 -0600

    Ejercicios Javascript_1

---
+++ ejerciciosJs.js
@@ -0,0 +1,186 @@
+//Part 1
+var x = false;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(x));
+
+
+var x = true;
+console.log(x);
+var y = Number(x)
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z))
+
+console.log(typeof(x));
+
+
+var x = 0;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+
+console.log(typeof(Boolean(x)));
+
+
+var x = 1;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+
+console.log(typeof(Boolean(x)));
+
+
+var x = "0";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+
+console.log(typeof(Boolean(x)));
+
+
+var x = "000";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+
+console.log(typeof(z));
+
+
+console.log(typeof(Boolean(x)));
+
+
+var x = "1";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(Boolean(x)));
+
+
+var x = NaN;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+
+console.log(typeof(Boolean(x)));
+
+
+var x = Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(Boolean(x)));
+
+
+var x = -Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+
+console.log(typeof(Boolean(x)));
+
+
+var x = "";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(Boolean(x)));
+
+
+var x = "20";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+
+console.log(typeof(Boolean(x)));
+
+
+var x = null;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(Boolean(x)));
+
+
+var x = undefined;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(Boolean(x)));
+
+//Part 2 Calculator
+
+var birthYear = 1984;
+var futureYear  = 2012;
+var age  = futureYear - birthYear;
+console.log('your possible ages are:  ' + age + ' or ' + (age - 1));
+
+
+// Information Card
+
+var name= "Arturo";
+var locati = "cdmx";
+var hobby = " Dance";
+
+console.log("Hi, my name is "+ name + ". I live in " +locati+ " and enjoy "+ hobby);
